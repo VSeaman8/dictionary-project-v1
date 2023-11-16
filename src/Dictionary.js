49bbb79337cc60b30612a0e821f9cd55111480d5
VSeaman8 <127307143+VSeaman8@users.noreply.github.com>
2023-11-16 15:41:22 +0000

    removed unnecessary console.log

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,36 +1,34 @@
 import React, { useState } from "react";
-import "./Dictionary.css";
 import axios from "axios";
-import Result from "./Results";
+import Results from "./Results";
+import "./Dictionary.css";
 
 export default function Dictionary() {
-  let { keyword, setKeyword } = useState("");
-  let { results, setResults } = useState(null);
+  let [keyword, setKeyword] = useState("");
+  let [results, setResults] = useState(null);
 
   function handleResponse(response) {
-    console.log(response.data[0]);
     setResults(response.data[0]);
   }
 
   function search(event) {
     event.preventDefault();
 
-    let apiURL = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
-    axios.get(apiURL).then(handleResponse);
+    // documentation: https://dictionaryapi.dev/e
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
+    axios.get(apiUrl).then(handleResponse);
   }
 
   function handleKeywordChange(event) {
-    console.log(event.target.value);
     setKeyword(event.target.value);
   }
 
   return (
     <div className="Dictionary">
       <form onSubmit={search}>
-        <input type="search" autoFocus={true} onChange={handleKeywordChange} />
-        <input type="submit" />
+        <input type="search" onChange={handleKeywordChange} />
       </form>
-      <Result results={results} />
+      <Results results={results} />
     </div>
   );
 }
