09347ada92179dd5319d6af187739dbc12786e9e
VSeaman8 <127307143+VSeaman8@users.noreply.github.com>
2023-11-21 10:35:23 +0000

    small text updates

--- src/Meaning.js
+++ src/Meaning.js
@@ -2,6 +2,7 @@ import React from "react";
 import Synonyms from "./Synonyms";
 
 export default function Meaning(props) {
+  console.log(props.meaning);
   return (
     <div className="Meaning">
       <h3>{props.meaning.partOfSpeech}</h3>
@@ -9,11 +10,10 @@ export default function Meaning(props) {
         return (
           <div key={index}>
             <p>
-              <strong> definition:</strong> {definition.definition}
+              <strong>Definition:</strong> {definition.definition}
               <br />
-              <strong>Example: </strong>
+              <strong>Example:</strong>
               <em>{definition.example}</em>
-              <br />
               <Synonyms synonyms={definition.synonyms} />
             </p>
           </div>
