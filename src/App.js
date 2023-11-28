65f983dc05d856855e9143d1c1d714e4e463e139
VSeaman8 <127307143+VSeaman8@users.noreply.github.com>
2023-11-28 11:30:51 +0000

    added style to dictionary.css; added functions to dictionary.js
    added functions:
    handleSubmit
    search
    added div hint

--- src/App.js
+++ src/App.js
@@ -5,17 +5,18 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <header>
+        <header className="App-header">
           <h1>What Word do you want to look up?</h1>
         </header>
+
         <main>
-          <Dictionary />
+          <Dictionary defaultKeyword="sunset" />
         </main>
         <footer className="text-center">
           <small>
             Coded by{" "}
             <a href="https://github.com/VSeaman8/dictionary-project-v1">
-              Vicki
+              Vicki Seaman
             </a>
           </small>
         </footer>
