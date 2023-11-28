4b1ee1a909823b59918c620f7a407d04f38579d7
VSeaman8 <127307143+VSeaman8@users.noreply.github.com>
2023-11-28 12:12:17 +0000

    added styling for meaning, phonetic and synonyms

--- src/App.js
+++ src/App.js
@@ -5,10 +5,6 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <header className="App-header">
-          <h1>What Word do you want to look up?</h1>
-        </header>
-
         <main>
           <Dictionary defaultKeyword="sunset" />
         </main>
